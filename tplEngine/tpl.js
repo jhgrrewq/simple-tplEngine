1f339715bc13fa28c42b0a36da11047e361aa3a3
jack <qwerrghj@sina.com.cn>
2018-03-13 11:03:00 +0800

    递减

--- tplEngine/tpl.js
+++ tplEngine/tpl.js
@@ -26,39 +26,53 @@ var escapes = {
 
 // text 传入的模板
 // data 渲染的数据
+// function template(text, data) {
+//     var index = 0; // 记录当前扫描到了哪里
+//     var function_string = '';
+//     text.replace(matcher, function(match, interpolate, evaluate, offset) {
+//         // match 匹配模式的字符串，
+//         // interpolate 为第1个子表达式匹配的字符串， 即 <%= %> 中的变量
+//         // evaluate 为第2个子表达式匹配的字符串， 即 <% %> 中的 js代码
+//         // offset 为匹配模式的字符串在text中的初始位置
+
+//         function_string += text.slice(index, offset)
+//             .replace(escaper, function(match) { return '\\' + escapes[match]; })
+
+
+//         // 如果是<%= %> 变量 拼接字符串
+//         if(interpolate){
+//             function_string += "' + " + interpolate + " + '";
+//         }
+//         // 如果是<% %> 变量 直接作为代码
+//         if(evaluate){
+//             function_string += "';" + evaluate + "temp += '";
+//         }
+//         // 递增index，跳出interpolate或evaluate
+//         index = offset + match.length;
+//     })
+
+//     // 最后拼接函数字符串，返回temp
+//     // 添加 with(expression) {statement}
+//     var tpl = "var temp = '';temp += '" + function_string + "';return temp;";
+//     console.log(tpl)
+//     // 通过function_string 生成函数 传入数据渲染
+//     var render = new Function('obj', tpl);
+//     return render(data)
+// }
+
 function template(text, data) {
-    var index = 0; // 记录当前扫描到了哪里
-    var function_string = "var temp = '';";
+    var function_string = text.replace(escaper, function(match) { return '\\' + escapes[match]; })
+        .replace(/<%=([\s\S]+?)%>/g, function(match, code) {
+            return "' + " + code + " + '" 
+        })
+        .replace(/<%([\s\S]+?)%>/g, function(match, code) {
+            return "';" + code + "temp += '" 
+        })
 
-    function_string += "temp += '";
-    text.replace(matcher, function(match, interpolate, evaluate, offset) {
-        // match 匹配模式的字符串，
-        // interpolate 为第1个子表达式匹配的字符串， 即 <%= %> 中的变量
-        // evaluate 为第2个子表达式匹配的字符串， 即 <% %> 中的 js代码
-        // offset 为匹配模式的字符串在text中的初始位置
-
-        function_string += text.slice(index, offset)
-            .replace(escaper, function(match) { return '\\' + escapes[match]; })
-
-
-        // 如果是<%= %> 变量 拼接字符串
-        if(interpolate){
-            function_string += "' + " + interpolate + " + '";
-        }
-        // 如果是<% %> 变量 直接作为代码
-        if(evaluate){
-            function_string += "';" + evaluate + "temp += '";
-        }
-        // 递增index，跳出interpolate或evaluate
-        index = offset + match.length;
-    })
-
-    // 最后代码是返回temp 也就是返回拼接好的字符串
-    function_string += "';return temp;";
-    console.log(function_string)
-
-    // 通过function_string 生成函数 传入数据渲染
-    var render = new Function('obj', function_string);
+    var tpl = "var temp = '';\nwith(obj || {}) {\ntemp += '" + function_string + "'\n};\nreturn temp;";
+    // 生成函数 传入数据渲染
+    var render = new Function('obj', tpl);
     return render(data)
 }
+
 
